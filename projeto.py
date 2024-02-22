48736ce0b9dc497f2893fcc584bab367b10dd818
Heitormac <heitor.santos@germinare.org.br>
2024-02-22 19:29:13 -0300

    Alteração do Projeto

--- projeto.py
+++ projeto.py
@@ -9,11 +9,11 @@ while acerto:
     chutes += 1
     numero_chutado = int(input("Chute um numero inteiro de 1 a 20:"))
     if numero_chutado == numero_aleatorio:
-        print("acertou!")
-        print(f"Numero de chutes: {chutes}")
+        print("acertou, parabens!!!")
+        print(f"Numero de tentativas: {chutes}")
         acerto = False
     elif numero_chutado < 1 or numero_chutado > 20:
-        print("Digite um numero válido!")
+        print("Digite um numero válido, de 1 a 20!")
     else:
         if numero_chutado > numero_aleatorio:
             print(f"O numero secreto é menor que {numero_chutado}")
