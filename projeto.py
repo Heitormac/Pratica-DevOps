af7851d23b1f7e7aabb594e1f68973b7df7396c3
Heitormac <heitor.santos@germinare.org.br>
2024-02-23 08:09:07 -0300

    QA para main

--- projeto.py
+++ projeto.py
@@ -1,10 +1,16 @@
+#Feitor por Henry
+
+#importando o randint da biblioteca random
 from random import randint
- 
+
+#variavel contador que vai mostrar a quantidade de tentativas
 chutes = 0
+
+#usando o randint da biblioteca random
 numero_aleatorio = int(randint(1,20))
  
 acerto = True
- 
+#while que vai aumentar a quantidade de tentativas e repetir o codigo até o usuario acertar o numero misterioso
 while acerto:
     chutes += 1
     numero_chutado = int(input("Chute um numero inteiro de 1 a 20: "))
@@ -19,3 +25,5 @@ while acerto:
             print(f"O numero secreto é menor que: {numero_chutado}")
         else:
             print(f"O numero secreto é maior que: {numero_chutado}")
+#fim do programa
+            
